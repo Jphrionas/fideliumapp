eb340c7e9a5820605a3fc0b4492b47ae687c239d
Jphrionas <36055405+Jphrionas@users.noreply.github.com>
2019-04-22 13:40:05 -0300

    Correct Impor.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 insert into role(role_id, public_id, nome, registration_date, disabled)  values (null, 
 'ffc774f5-d7fd-40c4-999e-88c258a8b9e4', 'ROLE_ADMIN', current_date(), false);
 
-insert into role(role_id, public_id, nome, registration_date)  values (null, '7ed44a6a-ee80-48a9-9d27-c2c6e51e1f96
+insert into role(role_id, public_id, nome, registration_date, disabled)  values (null, '7ed44a6a-ee80-48a9-9d27-c2c6e51e1f96
 ', 'ROLE_USER', current_date(), false);
 
 
